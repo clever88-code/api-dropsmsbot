105f06bb1f8458db8cc185ccf3ac21f2ea7dd171
clever88 <prostopavel2020@yandex.ru>
2023-03-17 18:35:35 +0300

    fixs

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import time
 
 #токет бота drop sms
 token = '67e8e9ad-9ac9-48bf-888d-f2cf9d21418b'
-#номера стараны
-id_number = '73'
-#название сервиса
+#номера страны, можно взять тут https://dropsms.ru/api.html
+id_number = '1'
+#название сервиса, можно взять тут https://dropsms.ru/api.html
 service = "vk"
 
+#делаем get запрос для получения баланса
 response = requests.get(f'https://api.dropsms.ru/stubs/handler_api.php?action=getBalance&api_key={token}')
 
+#получаем текуший баланс
 balance = response.text
 print(balance)
 
-
+#делаем get запрос для получение номера
 number = requests.get(f'https://api.dropsms.ru/stubs/handler_api.php?action=getNumber&api_key={token}&service={service}&country={id_number}')
 
 list_number = number.text
@@ -33,11 +35,17 @@ number_first = number_client[2]
 print(number_token)
 print(number_first)
 
+
+print('подтвердите когда вы впишите номер телефона(нажмите enter)')
+text = input()
+
+
 #получения кода в течении 10 минут
 for i in range(120):
+    #делаем get запрос для получение кода
     sms_code = requests.get(f'https://api.dropsms.ru/stubs/handler_api.php?action=getStatus&api_key={token}&id=(number_token)')
     sms = sms_code.text
-    print(sms)
+    print(i, 'запрос кода')
     time.sleep(5)
     if sms != "NO_ACTIVATION":
         exit()
